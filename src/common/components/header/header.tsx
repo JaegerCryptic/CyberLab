73c5301b2d70bdb3304aeef89a5a229240df3abe
Ganoush <74544922+Ganoush69@users.noreply.github.com>
2024-09-24 18:41:18 +1000

    Update src/common/components/header/header.tsx
    Co-authored-by: Kyle <39253934+JaegerCryptic@users.noreply.github.com>

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import '../../../theme/topbar.css';
 
-const TopBar = () => {
+export const TopBar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
