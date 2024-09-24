fbb8d54bb8d62e78bbd3b5d8bab4fdc5c093abb3
Zephrin sykes <redxxephos@gmail.com>
2024-09-24 18:40:23 +1000

    Route Removal/Comment Out Minor fixes Lobotomy.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import { Navigate, RouteObject, useLocation } from 'react-router-dom'
-import  Dashboard from '../features/dashboard/dashboard.tsx'
+// import  Dashboard from '../features/dashboard/dashboard.tsx'
 
 export const WORKSPACES_ROUTE_PATH = '/workspaces'
 
@@ -16,9 +16,9 @@ export const ROOT_ROUTE: RouteObject = {
   children: [],
 }
 
-// trying to define the dashboard as a proper route ... but im not sure this is the correct way to do it.
-export const DASHBOARD_ROUTE: RouteObject ={
-  path: '/dashboard',
-  element: <Dashboard />,
-  children: [],
-}
+// // trying to define the dashboard as a proper route ... but im not sure this is the correct way to do it.
+// export const DASHBOARD_ROUTE: RouteObject ={
+//   path: '/dashboard',
+//   element: <Dashboard />,
+//   children: [],
+// }
