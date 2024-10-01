c2beecf99f7b0935ca03c422efbc9c3b1f1c5397
Kyle Kent <kkent10@yahoo.com>
2024-10-01 15:36:07 +1000

    Refactor HomeRoutes to update dashboard routes import

--- src/routes/HomeRoutes.tsx
+++ src/routes/HomeRoutes.tsx
@@ -1,11 +1,9 @@
 import { RouteObject } from "react-router-dom"
 
 import { UserWorkspaceRoutesWrapper } from "../common/components/routing/UserWorkspaceRoutesWrapper"
-import { DASH_ROUTES } from "./DashboardRoutes"
-
-export const DASH_ROUTE_PATH = "/dashboard"
+import { DASHBOARD_ROUTES } from "./DashboardRoutes"
 
 export const HOME_ROUTES: RouteObject = {
 	element: <UserWorkspaceRoutesWrapper />,
-	children: [DASH_ROUTES],
+	children: [DASHBOARD_ROUTES],
 }
