66336edd302391438a849953c8cff1fb1544a92c
Kyle Kent <kkent10@yahoo.com>
2024-10-02 15:06:32 +1000

    Refactor AuthenticatedRoutesWrapper to use getRedirectPath helper for redirect logic

--- src/common/components/routing/AuthenticatedRoutesWrapper.tsx
+++ src/common/components/routing/AuthenticatedRoutesWrapper.tsx
@@ -1,24 +1,19 @@
 import { Navigate, useLocation } from "react-router-dom"
 import { useCookies } from "react-cookie"
 
-import {
-	WELCOME_ROUTE_PATH,
-	DASHBOARD_ROUTE_PATH,
-} from "../../../routes/startup/startupPaths"
 import { AuthenticatedUserWrapper } from "./AuthenticatedUserWrapper"
 import AuthenticationModal from "../modals/AuthenticationModal"
+import { getRedirectPath } from "./helpers"
 
 export const AuthenticatedRoutesWrapper = () => {
 	const [cookies] = useCookies(["userName"])
 	const userName = cookies.userName
 	const location = useLocation()
 
-	if (!userName && location.pathname !== WELCOME_ROUTE_PATH) {
-		return <Navigate to={WELCOME_ROUTE_PATH} replace />
-	}
+	const redirectPath = getRedirectPath(userName, location.pathname)
 
-	if (userName && location.pathname === WELCOME_ROUTE_PATH) {
-		return <Navigate to={DASHBOARD_ROUTE_PATH} replace />
+	if (redirectPath) {
+		return <Navigate to={redirectPath} replace />
 	}
 
 	return (
