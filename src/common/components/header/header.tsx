b19cfde6b9886b23a928e3ec2da9ae51a973d268
Kyle Kent <kkent10@yahoo.com>
2024-10-03 10:55:59 +1000

    Refactor topbar and sidebar components

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,30 +1,28 @@
-import React, { useState } from 'react';
+import { useState } from "react"
 
-import '../../../theme/topbar.css';
+import "../../../theme/topbar.css"
 
 export const TopBar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [dropdownOpen, setDropdownOpen] = useState(false)
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
+	const toggleDropdown = () => {
+		setDropdownOpen(!dropdownOpen)
+	}
 
-  // normal ass topbar that has a dropdown option
-  return (
-    <div className="topbar">
-      <h2>Dashboard</h2>
-      <div className="dropdown">
-        <button onClick={toggleDropdown}>
-        SCP Foundation
-        </button>
-        {dropdownOpen && (
-          <div className="dropdown-menu">
-            <p>SCP Foundation 1</p>
-            <p>SCP Foundation 2</p>
-            <p>SCP Foundation 3</p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+	// normal ass topbar that has a dropdown option
+	return (
+		<div className='topbar'>
+			<h2>Dashboard</h2>
+			<div className='dropdown'>
+				<button onClick={toggleDropdown}>SCP Foundation</button>
+				{dropdownOpen && (
+					<div className='dropdown-menu'>
+						<p>SCP Foundation 1</p>
+						<p>SCP Foundation 2</p>
+						<p>SCP Foundation 3</p>
+					</div>
+				)}
+			</div>
+		</div>
+	)
+}
--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,24 +1,20 @@
-import  Sidebar  from '../../common/components/sidebar/sidebar.tsx' // This is importing the sidebar that i am currently trying to work on.
-import  TopBar  from '../../common/components/header/header.tsx' // same story with the header/topbar 
-import  React from 'react'
+import { Sidebar } from "../../common/components/sidebar/sidebar.tsx"
+import { TopBar } from "../../common/components/header/header.tsx"
+import React from "react"
 
-
-
-// DASHBOARD
 const Dashboard: React.FC = () => {
-    return (
-      <div className="dashboard">
-        <TopBar />
-        <div className="dashboard-content">
-          <Sidebar />
-          <div className="content">
-            <h1>Dashboard Content</h1>
-            {/*  insert the rest of the games here i suppose. */}
-          </div>
-        </div>
-      </div>
-    );
-  };
+	return (
+		<div className='dashboard'>
+			<TopBar />
+			<div className='dashboard-content'>
+				<Sidebar />
+				<div className='content'>
+					<h1>Dashboard Content</h1>
+					{/*  insert the rest of the games here i suppose. */}
+				</div>
+			</div>
+		</div>
+	)
+}
 
-
-export default Dashboard;
+export default Dashboard
