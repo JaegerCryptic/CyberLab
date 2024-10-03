8b212cc4b103b7a3286866e257d4010da3b98a45
Kyle Kent <kkent10@yahoo.com>
2024-10-03 11:02:18 +1000

    Refactor DashboardRoutes to import Dashboard component from the correct path

--- src/routes/DashboardRoutes.tsx
+++ src/routes/DashboardRoutes.tsx
@@ -2,7 +2,7 @@ import { RouteObject } from "react-router-dom"
 
 import { DASHBOARD_ROUTE_PATH } from "./constants"
 import { UserWorkspaceRoutesWrapper } from "../common/components/routing/UserWorkspaceRoutesWrapper"
-import Dashboard from "../features/dashboard/dashboard"
+import { Dashboard } from "../features/dashboard/dashboard"
 
 export const DASHBOARD_ROUTES: RouteObject = {
 	path: DASHBOARD_ROUTE_PATH,
