b119218a497cfca9acb3f82f1230893ee8074b8f
Kyle Kent <kkent10@yahoo.com>
2024-10-03 13:46:32 +1000

    Refactor AuthenticatedRoutesWrapper component to simplify and improve routing functionality

--- src/common/components/routing/AuthenticatedRoutesWrapper.tsx
+++ src/common/components/routing/AuthenticatedRoutesWrapper.tsx
@@ -1,25 +1,14 @@
-import { Navigate, useLocation } from "react-router-dom"
-import { useCookies } from "react-cookie"
-
-import { AuthenticatedUserWrapper } from "./AuthenticatedUserWrapper"
-import AuthenticationModal from "../modals/AuthenticationModal"
-import { getRedirectPath } from "./helpers"
+import { Outlet } from "react-router-dom"
+import { Box } from "@mui/material"
 
 export const AuthenticatedRoutesWrapper = () => {
-	const [cookies] = useCookies(["userName"])
-	const userName = cookies.userName
-	const location = useLocation()
-
-	const redirectPath = getRedirectPath(userName, location.pathname)
-
-	if (redirectPath) {
-		return <Navigate to={redirectPath} replace />
-	}
-
 	return (
-		<>
-			<AuthenticationModal />
-			<AuthenticatedUserWrapper userId={userName} />
-		</>
+		<Box
+			overflow={"hidden"}
+			height={"100vh"}
+			sx={{ display: "flex", flexDirection: "column" }}
+		>
+			<Outlet />
+		</Box>
 	)
 }
