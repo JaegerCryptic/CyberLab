39f930a29bda7c6e0e967ebbfaa8fe90a6b6b618
Kyle Kent <kkent10@yahoo.com>
2024-10-07 11:45:20 +1000

    redo

--- src/common/components/sidebar/sidebar.tsx
+++ src/common/components/sidebar/sidebar.tsx
@@ -13,19 +13,11 @@ import { ExpandLess, ExpandMore, Menu as MenuIcon } from "@mui/icons-material"
 export const Sidebar = () => {
 	const [isOpen, setIsOpen] = useState(false)
 	const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-	const [isOpen, setIsOpen] = useState(false)
-	const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
 	const toggleSidebar = () => {
 		setIsOpen(!isOpen)
 	}
-	const toggleSidebar = () => {
-		setIsOpen(!isOpen)
-	}
 
-	const toggleDropdown = () => {
-		setIsDropdownOpen(!isDropdownOpen)
-	}
 	const toggleDropdown = () => {
 		setIsDropdownOpen(!isDropdownOpen)
 	}
