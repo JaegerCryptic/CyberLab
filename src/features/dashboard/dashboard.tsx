ae85a078e5a966b361d603f501fb0ec56ea7b209
Kyle Kent <kkent10@yahoo.com>
2024-10-08 12:38:37 +1000

    dsadas# Please enter the commit message for your changes. Lines starting

--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,7 +1,5 @@
 import { useCookies } from "react-cookie"
 
-import { useCookies } from "react-cookie"
-
 import { TopBar } from "../../common/components/header/header"
 import { useGame } from "../../routes/GameContext"
 import { PasswordChecker } from "./PasswordGame/PasswordChecker"
@@ -12,7 +10,6 @@ import { InformationHunterGame } from "./InformationHunter/InformationHunterGame
 export const Dashboard = () => {
 	const { selectedGame } = useGame()
 	const [cookies] = useCookies(["userName"])
-	const [cookies] = useCookies(["userName"])
 
 	return (
 		<div className='dashboard'>
@@ -22,7 +19,6 @@ export const Dashboard = () => {
 					<div className='content'>
 						{selectedGame === "Password Game" ? (
 							<PasswordGame />
-						) : selectedGame === "Password Checker" ? (
 						) : selectedGame === "Password Checker" ? (
 							<PasswordChecker />
 						) : selectedGame === "MD5 Hashing Cracking" ? (
