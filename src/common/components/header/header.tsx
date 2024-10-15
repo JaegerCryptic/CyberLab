830a3e79cc5826a61d9b2338022da0149b352593
Kyle <kkent10@yahoo.com>
2024-10-15 23:56:52 +1000

    Refactor header component to add tooltip for SCP Foundation button

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,18 +1,33 @@
 import { useState } from 'react'
-import { AppBar, Toolbar, Typography, Button, Menu, Box } from '@mui/material'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Menu,
+  Box,
+  Tooltip,
+  ClickAwayListener,
+} from '@mui/material'
 
 import { appTheme } from '../../../theme/style'
 import SCPLogo from '../../../images/scp_logo.png'
+import SCPLogoAlt from '../../../images/scp_logo_alt.png'
 
 export const TopBar = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [tooltipOpen, setTooltipOpen] = useState(false)
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
+    setTooltipOpen(true)
   }
 
   const handleClose = () => {
-    setAnchorEl(null)
+    setTooltipOpen(false)
+  }
+
+  const handleClickAway = () => {
+    setTooltipOpen(false)
   }
 
   return (
@@ -30,9 +45,31 @@ export const TopBar = () => {
           <img src={SCPLogo} style={{ marginRight: 0, height: '20px' }} />
           <Typography variant='inherit'>SCP Foundation</Typography>
         </Box>
-        <Button color='inherit' onClick={handleClick}>
-          About SCP Foundation
-        </Button>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <Tooltip
+              title='Learn more about the SCP Foundation'
+              open={tooltipOpen}
+              onClose={handleClose}
+              disableHoverListener
+              disableFocusListener
+            >
+              <Button color='inherit' onClick={handleClick}>
+                About SCP Foundation
+              </Button>
+            </Tooltip>
+          </ClickAwayListener>
+          <img
+            src={SCPLogoAlt}
+            style={{ marginLeft: '8px', height: '40px', width: '60px' }}
+          />
+        </Box>
         <Menu
           anchorEl={anchorEl}
           open={Boolean(anchorEl)}
